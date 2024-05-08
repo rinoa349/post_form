91673bc96155db29100db090ea66872e35ac9839
rinoa349 <chan2s@icloud.com>
2024-05-08 17:16:48 +0900

    check編集

---
+++ sign_in/check.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>会員登録</title>
+</head>
+<body>
+  <div id="wrap">
+    <div id="head">
+      <h1>確認画面</h1>
+    </div>
+  </div>
+</body>
+</html>
