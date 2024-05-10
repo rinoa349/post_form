d8a1f4fc846e01852ad4a9eeea9d34f38cefc554
rinoa349 <chan2s@icloud.com>
2024-05-10 16:50:00 +0900

    update編集開始

---
+++ update.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>投稿掲示板</title>
+</head>
+<body>
+  <div id="wrap">
+    <div id="head">
+      <h1>編集完了画面</h1>
+    </div>
+    <div id="content">
+      <p>編集が完了しました。</p>
+      <p><a href="index.php">&laquo;一覧に戻る</p>
+    </div>
+  </div>
+</body>
+</html>
