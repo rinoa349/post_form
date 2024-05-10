7e2249a82f9761d8801f95f4974023a907417c9c
rinoa349 <chan2s@icloud.com>
2024-05-10 16:07:29 +0900

    view編集

---
+++ view.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+require('library.php');
+
+if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
+  $id = $_SESSION['id'];
+  $name = $_SESSION['name'];
+} else {
+  header('Location: login.php');
+  exit();
+}
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+if (!$id) {
+  header('Location: index.php');
+  exit();
+}
+
+$db = dbconnect();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投稿掲示板</title>
+</head>
+<body>
+  <div id="wrap">
+    <div id="head">
+      <h1>投稿掲示板</h1>
+    </div>
+    <div id="content">
+      <p>&laquo;<a href="index.php">一覧に戻る</p>
+        <?php
+          $stmt = $db->prepare('select p.id, p.member_id, p.message, p.created, m.name from posts p, members m where p.id=? and m.id=p.member_id order by id desc');
+          if (!$stmt) {
+            die($db->error);
+          }
+          $stmt->bind_param('i', $id);
+          $success = $stmt->execute();
+          if (!$success) {
+            die($db->error);
+          }
+          $stmt->bind_result($id, $member_id, $message, $created, $name);
+          if ($stmt->fetch()) :
+        ?>
+      <div class="msg">
+        <p><?php echo h($message); ?><span class="name">（<?php echo h($name); ?>）</span></p>
+            <p class="day"><a href="view.php?id=<?php echo h($created); ?>"></a>
+              [<a href="delete.php?id=<?php echo h($id); ?>" style="color: #F33;">削除</a>] | 
+              [<a href="edit.php?id=<?php echo h($id); ?>" style="color: #F33;">編集</a>]
+            </p>
+      </div>
+      <?php else : ?>
+        <p>その投稿は削除されたか、URLが間違いのため表示できません。</p>
+      <?php endif; ?>
+    </div>
+  </div>
+</body>
+</html>
